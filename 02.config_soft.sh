91441a71e28aec71e26bcf0cecb888e3d5450722
Vladimir Mashkov <vladimir@mashkov.com>
2022-05-01 10:53:22 +0300

    some updates

--- 02.config_soft.sh
+++ 02.config_soft.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-read -t 1 -p "disable firewalld"
+read -t 1 -p "========== disable firewalld =========="
+
 systemctl disable firewalld
 systemctl stop firewalld
 
-read -t 1 -p "enable iptables"
+read -t 1 -p "========== enable iptables =========="
+
 touch /etc/sysconfig/iptables
 touch /etc/sysconfig/iptables6
 systemctl start iptables
@@ -11,13 +13,7 @@ systemctl start ip6tables
 systemctl enable iptables
 systemctl enable ip6tables
 
-read -t 1 -p "install csf"
-cur_dir=$(pwd)
-cd /opt/csf/
-sh install.sh 
-cd $cur_dir
-
-read -t 1 -p "config csf"
+read -t 1 -p "========== config csf =========="
 
 sed -i 's/ICMP_IN = "1"/ICMP_IN = "0"/g' /etc/csf/csf.conf
 sed -i 's/TESTING = "1"/TESTING = "0"/g' /etc/csf/csf.conf
@@ -26,6 +22,6 @@ sed -i 's/TESTING_INTERVAL = "5"/TESTING_INTERVAL = "0"/g' /etc/csf/csf.conf
 sed -i 's/TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"/TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,593,993,995"/g' /etc/csf/csf.conf
 sed -i 's/TCP_OUT = "20,21,22,25,53,80,110,113,443,587,993,995"/TCP_OUT = "20,21,22,25,53,80,110,113,443,587,593,993,995"/g' /etc/csf/csf.conf
 
-read -t 1 -p "extract database ip_checker.tar.gz"
+read -t 1 -p "========== extract database ip_checker.tar.gz =========="
 tar -xzf ip_checker.tar.gz
 
--- 04.start_soft.sh
+++ 04.start_soft.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-read -t 1 -p "update repo"
+read -t 1 -p "========== update repo =========="
+
 yum update -y && yum upgrade -y
 
-read -t 1 -p "restart csf"
+read -t 1 -p "========== restart csf =========="
+
 csf -r
 sysctl -p
 
+read -t 1 -p "========== docker stop =========="
 
-
-read -t 1 -p "docker stop"
 systemctl stop docker
 systemctl stop docker.sock
 
-read -t 3 -p "docker start"
+read -t 3 -p "========== docker start =========="
+
 systemctl start docker.sock
 
-read -t 3 -p "docker restart"
+read -t 3 -p "========== docker restart =========="
+
 systemctl restart docker
 
 systemctl enable docker-compose-ocserv
@@ -24,6 +27,7 @@ systemctl start docker-compose-ocserv
 docker-compose up -d
 
 echo "========== To show a status =========="
+
 systemctl status iptables
 systemctl status ip6tables
 systemctl status csf
